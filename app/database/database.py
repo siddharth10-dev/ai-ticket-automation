cd91e92054eb27f3e1027997292a94f7f109bb5f
siddharth adep <siddharthadep@siddharths-MacBook-Pro.local>
2026-06-23 13:17:53 +0530

    Move database configuration to environment variables

--- app/database/database.py
+++ app/database/database.py
@@ -1,7 +1,12 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-DATABASE_URL = "postgresql://siddharthadep@localhost:5432/ai_ticket_automation"
-engine=create_engine(DATABASE_URL)
-SessionLocal=sessionmaker(bind=engine)
+from dotenv import load_dotenv
 
-db=SessionLocal()
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(bind=engine)
